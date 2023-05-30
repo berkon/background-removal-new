fe2f910225c8646454a08709bbb1daeed710fd96
Bernd Konnerth <bernd.konnerth@atos.net>
2023-05-30 16:55:50 +0200

    Now using OffscreenCanvases only

--- renderer.js
+++ renderer.js
@@ -1,11 +1,8 @@
-//import vision from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
 import vision from "./node_modules/@mediapipe/tasks-vision/vision_bundle.js";
-const { ImageSegmenter, SegmentationMask, FilesetResolver } = vision;
+const { ImageSegmenter } = vision;
 
 const VIDEO_WIDTH = 1280;
 const VIDEO_HEIGHT = 720;
-
-//FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm").then(async (vision) => {
 
 (async () => {
 	let vision = {
@@ -13,31 +10,28 @@ const VIDEO_HEIGHT = 720;
 		wasmLoaderPath: "./node_modules/@mediapipe/tasks-vision/wasm/vision_wasm_internal.js"
 	}
 
-   	//const imageSegmenter = await ImageSegmenter.createFromModelPath(vision, "https://storage.googleapis.com/mediapipe-tasks/image_segmenter/selfie_segmentation.tflite");
 	const imageSegmenter = await ImageSegmenter.createFromOptions(vision, {
-    	baseOptions: {
-			modelAssetPath:
-        	//	"https://storage.googleapis.com/mediapipe-assets/selfie_segm_128_128_3.tflite?generation=1678818071447437"
-        	//	"https://storage.googleapis.com/mediapipe-assets/selfie_segm_144_256_3.tflite?generation=1678818074421716"
-        	//"https://storage.googleapis.com/mediapipe-tasks/image_segmenter/selfie_segmentation.tflite"
-			"./selfie_segmentation.tflite"
-		},
+    	baseOptions: { modelAssetPath:	"./selfie_segmentation.tflite" },
 		runningMode: "VIDEO",
 		outputCategoryMask: true,
 		outputConfidenceMasks: true
     });
 
 	const video = document.getElementById("webcam");
+
     const canvasElement = document.createElement("canvas");
     canvasElement.width = VIDEO_WIDTH;
     canvasElement.height = VIDEO_HEIGHT;
     document.body.appendChild(canvasElement);
-    const canvasCtx = canvasElement.getContext("2d");
-    const bkgCanvasElement = document.createElement("canvas");
-    bkgCanvasElement.width = VIDEO_WIDTH;
-    bkgCanvasElement.height = VIDEO_HEIGHT;
-    const bkgCanvasCtx = bkgCanvasElement.getContext("2d");
-    const numOfPixels = VIDEO_WIDTH * VIDEO_HEIGHT;
+	const canvasCtx = canvasElement.getContext("bitmaprenderer");
+
+	const offscreenBkgCanvas = new OffscreenCanvas(VIDEO_WIDTH, VIDEO_HEIGHT);
+    const offscreenBkgCanvasCtx = offscreenBkgCanvas.getContext("2d");
+
+	const offscreenCanvas = new OffscreenCanvas(VIDEO_WIDTH, VIDEO_HEIGHT);
+	const offscreenCanvasCtx = offscreenCanvas.getContext("2d");
+
+	const numOfPixels = VIDEO_WIDTH * VIDEO_HEIGHT;
     const maskClampedArray = new Uint8ClampedArray(4 * numOfPixels);
 
     async function frameCallback(result) {
@@ -48,23 +42,25 @@ const VIDEO_HEIGHT = 720;
 			maskClampedArray[i * 4 + 3] = 255 - result.categoryMask.containers[0][i];
 		}
 
-		bkgCanvasCtx.globalCompositeOperation = "copy";
-		bkgCanvasCtx.drawImage ( video, 0, 0, canvasElement.width, canvasElement.height );
+		offscreenBkgCanvasCtx.globalCompositeOperation = "copy";
+		offscreenBkgCanvasCtx.drawImage ( video, 0, 0, canvasElement.width, canvasElement.height );
 		const maskImageData = new ImageData( maskClampedArray, VIDEO_WIDTH, VIDEO_HEIGHT );
 		const maskImageBitmap = await createImageBitmap(maskImageData);
 
 		// Draw mask shape onto the background canvas and blur that canvas
-		bkgCanvasCtx.globalCompositeOperation = "destination-out"; // keep only part where not overlapping with mask
-		bkgCanvasCtx.drawImage ( maskImageBitmap, 0, 0, canvasElement.width, canvasElement.height );
-		bkgCanvasCtx.filter = "blur(10px)";
+		offscreenBkgCanvasCtx.globalCompositeOperation = "destination-out"; // keep only part where not overlapping with mask
+		offscreenBkgCanvasCtx.drawImage ( maskImageBitmap, 0, 0, canvasElement.width, canvasElement.height );
+		offscreenBkgCanvasCtx.filter = "blur(10px)";
 
 		// Draw the content from the hidden (blurred) canvas onto the output canvas.
-		canvasCtx.globalCompositeOperation = "copy";
-		canvasCtx.drawImage ( bkgCanvasElement, 0, 0 );
+		offscreenCanvasCtx.globalCompositeOperation = "copy";
+		offscreenCanvasCtx.drawImage ( offscreenBkgCanvas, 0, 0 );
 
 		// Draw the original video frame onto the output canvas.
-		canvasCtx.globalCompositeOperation = "destination-atop"; // draw behind and shine through
-		canvasCtx.drawImage ( video, 0, 0, canvasElement.width, canvasElement.height );
+		offscreenCanvasCtx.globalCompositeOperation = "destination-atop"; // draw behind and shine through
+		offscreenCanvasCtx.drawImage ( video, 0, 0, canvasElement.width, canvasElement.height );
+
+		canvasCtx.transferFromImageBitmap(offscreenCanvas.transferToImageBitmap());
 
 		window.requestAnimationFrame(triggerSegmentation);
     }
